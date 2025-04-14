a17d246c18eba42faad7f4b891a6a868846e1f48
quantumphp123 <rashad.quantumitinnovation@gmail.com>
2025-04-14 09:56:31 +0530

    content update

--- resources/views/emails/login_reminder.blade.php
+++ resources/views/emails/login_reminder.blade.php
@@ -1,10 +1,19 @@
 <p>Dear {{ $fullName }},</p>
 
-<p>As per our records, you have not logged into HRMS yet. Please note that the maximum login time is 10:30 AM,
-and it is now past that time, but you have not yet logged in. Kindly ensure that you log in immediately to mark your attendance.</p>
+<p>As per our records, you have either not logged into the HRMS system today.</p>
 
-<p>For any concerns, please reach out to the HR team.</p>
+<p>Please note that the maximum allowed time to log in is 10:30 AM, and it is now past that. We kindly request you to
+    log in immediately if you haven't already. If you have not logged in, please make sure to avoid late check-ins to
+    stay
+    compliant with attendance policies.</p>
+
+<p>For any concerns or if you believe this message was sent in error, feel free to reach out to the HR team.</p>
+
+<p>Thank you for your attention and cooperation.</p>
+
+<p><strong>Note: Please ensure your availability on "Teams" as per office timing.</strong></p>
 
 <br>
+
 <p>Regards,</p>
 <p>HR Team</p>
