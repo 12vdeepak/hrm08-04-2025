d58e834818a1e24017279b95513f8d3b2bd7aa18
quantumphp123 <rashad.quantumitinnovation@gmail.com>
2025-04-29 08:49:33 +0530

    latest code

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('send:login-reminder')->weekdays()->at('10:31');
         $schedule->command('send:daily-status-report')->weekdays()->at('11:00');
         $schedule->command('send:late-mark-reminder')->weekdays()->at('20:00');
+        $schedule->command('emails:send-birthday-wishes')->dailyAt('08:00');
        
     }
 
