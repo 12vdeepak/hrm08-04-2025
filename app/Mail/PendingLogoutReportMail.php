01da859852ae981a29fe15a4619910a8b4f716f2
deepak <deepak.quantumitinnovation@gmail.com>
2025-07-17 16:31:30 +0530

    update mailable class

--- app/Mail/PendingLogoutReportMail.php
+++ app/Mail/PendingLogoutReportMail.php
@@ -3,10 +3,7 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class PendingLogoutReportMail extends Mailable
@@ -24,40 +21,13 @@ class PendingLogoutReportMail extends Mailable
     }
 
     /**
-     * Get the message envelope.
+     * Build the message.
      */
-    public function envelope(): Envelope
-    {
-        return new Envelope(
-            subject: 'Pending Logout Report Mail',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            markdown: 'emails.pending_logout_report',
-        );
-    }
-
     public function build()
     {
         return $this->subject('Daily Logout Pending Employees report')
-            ->to(['hr@quantumitinnovation.com', 'mansi@quantumitinnovation.com', 'sanchitha@quantumitinnovation.com'])
+            ->to(['deepak.quantumitinnovation@gmail.com'])
             ->view('emails.daily_logout_pending_report')
             ->with(['pendingUsers' => $this->pendingUsers]);
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
     }
 }
