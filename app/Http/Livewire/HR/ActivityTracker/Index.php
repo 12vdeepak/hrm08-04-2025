70134b467f1f883d2c5b8276f71302fdf1b46c8a
deepak <deepak.quantumitinnovation@gmail.com>
2025-07-30 18:10:24 +0530

    latest code

--- app/Http/Livewire/HR/ActivityTracker/Index.php
+++ app/Http/Livewire/HR/ActivityTracker/Index.php
@@ -24,7 +24,7 @@ class Index extends Component
 
     public function render()
     {
-        
+
         // $paginatedEmployees = User::whereNotIn('role_id', [1])
         //     ->when($this->search, function ($query, $search) {
         //         $query->where(function ($query) use ($search) {
@@ -33,14 +33,15 @@ class Index extends Component
         //         });
         //     })->paginate(2);
             $query = User::query()->whereNotIn('role_id', [1]);
+            $query->where('employee_status', 1); // Filter only active employees
             if($this->search){
                 $query->where('name', 'like', "%{$this->search}%");
                 $query->orWhere('lastname', 'like', "%{$this->search}%");
             }
 
             $paginatedEmployees = $query->paginate(10);
-            
-            
+
+
         return view('livewire.h-r.activity-tracker.index', compact('paginatedEmployees'));
     }
 
@@ -57,12 +58,12 @@ class Index extends Component
             ->whereDate('activity_time', $this->date)
             ->selectRaw('SUM(TIME_TO_SEC(TIMEDIFF(end_time, start_time))) as total_seconds')
             ->value('total_seconds');
-    
+
         $hours = floor($totalSeconds / 3600);
         $minutes = floor(($totalSeconds - ($hours * 3600)) / 60);
         $seconds = $totalSeconds - ($hours * 3600) - ($minutes * 60);
         $formattedTime = sprintf('%02d:%02d:%02d', $hours, $minutes, $seconds);
-    
+
         return $formattedTime;
     }
 }
